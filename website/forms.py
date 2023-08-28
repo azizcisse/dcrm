bb0f02bfcc3eb1489b6b1e3dcaaacd52ab740977
azizcisse <alaziz6c78@gmail.com>
2023-08-28 17:58:52 +0000

    Deuxieme commit

---
+++ website/forms.py
@@ -0,0 +1,36 @@
+from typing import Any
+from django import forms
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.models import User
+
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(label="", max_length="120", widget=forms.TextInput(attr={'class':'form-control', 'placeholder':'Adresse email'}))
+    first_name = forms.CharField(label="", max_length="100", widget=forms.TextInput(attr={'class':'form-control', 'placeholder':'Votre Prénom'}))
+    last_name = forms.CharField(label="", max_length="100", widget=forms.TextInput(attr={'class':'form-control', 'placeholder':'Votre Nom'}))
+    
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1' 'password2')    
+        
+	
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
+    
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['placeholder'] = 'Nom d\'Utilisateur'
+        self.fields['username'].label = ''
+        self.fields['username'].help_text = '<span class="form-text text-muted"><small>150 caractères ou moins requis. Lettres, chiffres et @/./+/-/_ uniquement.</small></span>'
+
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Mot de Passe'
+        self.fields['password1'].label = ''
+        self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Votre mot de passe ne peut pas être trop similaire à vos autres informations personnelles.</li><li>Votre mot de passe doit contenir au moins 8 caractères.</li><li>Votre mot de passe ne peut pas être un mot de passe couramment utilisé.</li><li>Votre mot de passe ne peut pas être entièrement numérique.</li></ul>'
+        
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['placeholder'] = 'Confirmer Mot de Passe'
+        self.fields['password2'].label = ''
+        self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Entrez le même mot de passe que précédemment, pour vérification.</small></span>'
+    
--- website/views.py
+++ website/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    #Vérification pour voir si tu es connecté
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        # Si tu es authentifié
+        user = authenticate(request, username=username, password=password) 
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Félicitation vous etes conectés!!!")
+            return redirect('home')
+        else:
+            messages.success(request, "Une erreur s'est produite lors de la connexion, veuillez réessayer...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+
+
+
+def logout_user(request):
+        logout(request) 
+        messages.success(request, "Vous avez été déconnecté...")
+        return redirect('home')
+ 
+ 
+def register_user(request):
+     return render(request, 'home.html', {})
