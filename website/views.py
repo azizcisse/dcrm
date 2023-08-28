98f2a89a2d7418f0adc0fa9f6554963633144e33
azizcisse <alaziz6c78@gmail.com>
2023-08-28 19:07:57 +0000

    Troisième commit

--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from website.forms import SignUpForm
 
 
 def home(request):
@@ -30,4 +31,18 @@ def logout_user(request):
  
  
 def register_user(request):
-     return render(request, 'home.html', {})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            #Il s'authentifie et se connecte
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Félicitations, Inscription réussie, Bienvenue au CRM-DJANGO")
+            return redirect('home') 
+    else:
+         form = SignUpForm()           
+         return render(request, 'register.html', {'form':form})
+    return render(request, 'register.html', {'form':form})
